282b82872b3dcd8599a261f91ac2e68aa74c70b4
Joshua Cadavez <cadav001@umn.edu>
2020-04-23 10:14:07 -0400

    Added Hero clickability

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,12 +10,15 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
   heroes = HEROES;
+  // assigns the Hero property
+  selectedHero: Hero;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
